5438a6310219ec68b7c42818c3f6d7a5fde79a67
WebAppCourse <marian.o.seliuchenk@lpnu.ua>
2024-01-12 18:56:53 +0200

    Updated the hello world response text

--- app.py
+++ app.py
@@ -3,4 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, World! Now my app is running locally!!!! Yeah!!!!'
+
+if __name__ == "__main__":
+    app.run();
